da5b95d2a0d02aeb8b6497bac207135540e49ebd
Kim Chi <kimchilethi411@gmail.com>
2021-04-17 21:32:36 +0700

    complete

--- btJS_2/script.js
+++ btJS_2/script.js
@@ -57,27 +57,56 @@ document.getElementById("inputForm").addEventListener("submit", (event) => {
         document.forms[0].reset();
         localStorage.setItem('data', JSON.stringify(array));
         showItem();
+        document.getElementById("nameWarning").innerText="";
+        document.getElementById("cateWarning").innerText="";
+        document.getElementById("imgWarning").innerText="";
+        document.getElementById("loadImage").src="";
     }
 });
-
+var m=0;
 function removeItem(x) {
-    x = x+2;
+    x=x+2-m;
     let  l = document.getElementById('table').rows.length;
-    for(let j=2; j< l ; j++) {
+    console.log(x,l);
+    for(let j=2; j<= l ; j++) {
         if(j==(x)) {
-            document.getElementById('table').deleteRow(j);
+            document.getElementById('table').deleteRow(x);
         }
     }
+    m++;
 }
 
 function Save(x)
 {
-    let j=x+2;
-    let name = document.getElementById(`itemNameNew${x}`);
+    let name = document.getElementById(`itemName${x}`);
     let value = document.getElementById(`itemNameNew${x}`).value;
-    name.innerHTML=`<td id="itemName${x}">${value}</td>`;
-    let itemEdit = name.innerHTML;
-    table.rows[j].cells[1].innerHTML = itemEdit;
+    name.innerHTML=value;
+
+    let cate = document.getElementById(`category${x}`);
+    let value2 = document.getElementById(`categoryNew${x}`).value;
+    cate.innerHTML=value2;
+    
+    let btnC = document.getElementById(`btnD${x}`);
+    btnC.innerHTML= `<td id="btnD${x}"><button onclick="removeItem(${x})">Delete</button></td>`;
+
+    let btnS = document.getElementById(`btnE${x}`);
+    btnS.innerHTML= `<td id="btnE${x}"><button onclick="editItem(${x})">Edit</button></td>`;
+}
+
+function Cancel(x) {
+    let name = document.getElementById(`itemName${x}`);
+    let value = document.getElementById(`itemName${x}`).value;
+    name.innerHTML=value;
+
+    let cate = document.getElementById(`category${x}`);
+    let value2 = document.getElementById(`category${x}`).value;
+    cate.innerHTML=value2;
+    
+    let btnC = document.getElementById(`btnD${x}`);
+    btnC.innerHTML= `<td id="btnD${x}"><button onclick="removeItem(${x})">Delete</button></td>`;
+
+    let btnS = document.getElementById(`btnE${x}`);
+    btnS.innerHTML= `<td id="btnE${x}"><button onclick="editItem(${x})">Edit</button></td>`;
 }
 
 function editItem(x) {
@@ -88,8 +117,18 @@ function editItem(x) {
     let itemNameEdit = name.innerHTML;
     table.rows[j].cells[1].innerHTML = itemNameEdit;
 
+    let category=document.getElementById(`category${x}`);
+    category.innerHTML=`<select class="custom-select mr-sm-2" id="categoryNew${x}">
+                        <option selected>Choose...</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>`;
+    let itemCateEdit = category.innerHTML;
+    table.rows[j].cells[2].innerHTML = itemCateEdit;
+
     let btnC = document.getElementById(`btnD${x}`);
-    btnC.innerHTML= `<td id="btnCancel${x}"><button onclick="Cancel(${x}, ${document})">Cancel</button></td>`;
+    btnC.innerHTML= `<td ><button onclick="Cancel(${x})">Cancel</button></td>`;
     let btnCEdit = btnC.innerHTML;
     table.rows[j].cells[4].innerHTML = btnCEdit;
 
@@ -112,41 +151,36 @@ function showItem () {
             </td>
             <td id="btnD${i}"><button onclick="removeItem(${i})">Delete</button></td>
             <td id="btnE${i}"><button onclick="editItem(${i})">Edit</button></td>
-        </tr>
-    `
+        </tr>`
     i++;
 }
 
-function uploadImage () {
-    let res = window.localStorage.getItem('myfile');
-    var image = new Image();
-    image.src = res;
-    document.getElementById("loadImage").src = res;
-    parentDiv.appendChild(image);
-}
-
 function validateForm() {
+    document.getElementById("nameWarning").innerHTML="";
+    document.getElementById("imgWarning").innerHTML="";
+    document.getElementById("cateWarning").innerHTML="";
+    let bien =true;
     let x = document.forms["inputForm"]["itemName"].value;
-    if (x == "") {
-        document.getElementById("nameWarning").innerHTML=("Name must be filled out");
-        return false;
-    }
-    if(!(x.length == 0 && x.length > 10 && ( x >= '0' && x <= '9' ) )){
-
-        document.getElementById("nameWarning").innerHTML=null;
+    if(!( x[0] < '0' || x[0] > '9') ) {
+        document.getElementById("nameWarning").innerHTML="Khong the bat dau bang so";
+        bien= false;
     }
     if(x.length>10){
         document.getElementById("nameWarning").innerHTML="Length must be less than 10";
-        return false;
+        bien= false;
+    }
+    if (x.toString().trim() == "") {
+        document.getElementById("nameWarning").innerHTML=("Name must be filled out");
+        bien= false;
     }
     if ( document.forms["inputForm"]["myfile"].value == "") {
-        document.getElementById("nameWarning").innerHTML=("File must be filled out");
-        return false;
+        document.getElementById("imgWarning").innerHTML=("File must be filled out");
+        bien= false;
     }
     if ( document.forms["inputForm"]["category"].value == "Choose...") {
-        document.getElementById("nameWarning").innerHTML=("Category must be choosen");
-        return false;
+        document.getElementById("cateWarning").innerHTML=("Category must be choosen");
+        bien= false;
     }
-
+    if(bien == false) return false;
     return true;
 }
